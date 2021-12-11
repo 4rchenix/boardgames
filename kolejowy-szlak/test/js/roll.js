743e2ad19384cf67207218f249284fc514c756e9
root <root@archenix.plarchenix.pl>
2021-12-11 03:33:38 +0100

    railroad H0resT new code modified image tree commit

---
+++ kolejowy-szlak/test/js/roll.js
@@ -0,0 +1,23 @@
+		function roll() {
+			document.getElementById("lower").style.display = "block";
+			document.getElementById("reroll").style.display = "inline-block";
+			//Clear the board
+			//Fetch all dice sets based on constant in file checkboxes.js
+			for(i=0; i < checkboxes.length; i++){
+				for(c=1; c < 5; c++){
+					for (j = 1; j < 7; j++) {
+						document.getElementById("checkbox_" + checkboxes[i] + "_" + c +"_" + j).style.display = "none";
+					}
+				}
+			}
+
+			//Fetch all dice sets based on constant in file checkboxes.js
+			for(i=0; i < checkboxes.length; i++){
+				if (document.getElementById(checkboxes[i]).checked){
+					for (j = 1; j < 5; j++) {
+						dice = Math.floor(Math.random() * 6) + 1;
+						document.getElementById("checkbox_" + checkboxes[i] + "_" + j +"_" + dice).style.display = "block";
+					}
+				}
+			}
+		}
