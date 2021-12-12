ba6dd2e77443f923ec1264b3e79beea8d97d3c56
root <root@archenix.plarchenix.pl>
2021-12-12 05:24:44 +0100

    railroad image symlink for arcade

--- kolejowy-szlak/test/js/checkboxes.js
+++ kolejowy-szlak/test/js/checkboxes.js
@@ -1,62 +1,40 @@
-
-const checkboxes = [
-	"base",
-//	"arcade",
-	"arcade/galactic-invaders",
-	"arcade/pluck-man",
-	"arcade/rainbow",
-	"arcade/tetromino",
-//	"blazing-red",
-	"blazing-red/lava",
-	"blazing-red/meteor",
-//	"deep-blue",
-	"deep-blue/lake",
-	"deep-blue/river",
-//	"eldritch",
-	"eldritch/investigation",
-	"eldritch/portal",
-	"eldritch/ritual",
-	"eldritch/tentacles",
-//	"electricity",
-	"electricity/power-grid",
-	"electricity/street-lamps",
-//	"engineer",
-	"engineer/construction",
-	"engineer/renovation",
-	"engineer/separation",
-	"engineer/special",
-//	"futuristic",
-	"futuristic/alien-farmer",
-	"futuristic/city-builder",
-	"futuristic/superconnection",
-//	"lush-green",
-	"lush-green/forest",
-	"lush-green/trails",
-//	"shining-yellow",
-	"shining-yellow/canyon",
-	"shining-yellow/desert",
-//	"sky",
-	"sky/airport",
-	"sky/weather",
-	"underground"
-]
-
-for(i=0; i < checkboxes.length; i++){
+const checkboxes = {
+	"base": ["base"],
+	"arcade": ["galactic-invaders", "pluck-man", "rainbow", "tetromino"],
+	"blazing-red": ["lava", "meteor"],
+	"deep-blue": ["lake", "river"],
+	"eldritch": ["investigation", "portal", "ritual", "tentacles"],
+	"electricity": ["power-grid", "street-lamps"],
+	"engineer": ["construction", "renovation", "separation", "special"],
+	"futuristic": ["alien-farmer", "city-builder", "superconnection"],
+	"lush-green": ["forest", "trails"],
+	"shining-yellow": ["canyon", "desert"],
+	"sky": ["airport", "weather"],
+	"underground": ["underground"],
+}
+for (var entry of Object.entries(checkboxes)) {
 	div = document.getElementById("checkboxes");
 	div.innerHTML = div.innerHTML + `
-	<label class="toggle" for="${checkboxes[i]}">
-		<input type="checkbox" class="toggle__input" id="${checkboxes[i]}" />
-		<span class="toggle-track">
-			<span class="toggle-indicator">
-				<!-- 	This check mark is optional	 -->
-				<span class="checkMark">
-					<svg viewBox="0 0 24 24" id="ghq-svg-check" role="presentation" aria-hidden="true">
-						<path d="M9.86 18a1 1 0 01-.73-.32l-4.86-5.17a1.001 1.001 0 011.46-1.37l4.12 4.39 8.41-9.2a1 1 0 111.48 1.34l-9.14 10a1 1 0 01-.73.33h-.01z"></path>
-					</svg>
-				</span>
+		<div>
+			<h3 class="toggle-title">${entry[0]}</h3>
+			${dice(entry[1])}
+		</div>	
+	`
+}
+
+function dice(dices) {
+	return dices.map(dice => (`<label class="toggle" for="${dice}">
+	<input type="checkbox" class="toggle__input" id="${dice}" />
+	<span class="toggle-track">
+		<span class="toggle-indicator">
+			<!-- 	This check mark is optional	 -->
+			<span class="checkMark">
+				<svg viewBox="0 0 24 24" id="ghq-svg-check" role="presentation" aria-hidden="true">
+					<path d="M9.86 18a1 1 0 01-.73-.32l-4.86-5.17a1.001 1.001 0 011.46-1.37l4.12 4.39 8.41-9.2a1 1 0 111.48 1.34l-9.14 10a1 1 0 01-.73.33h-.01z"></path>
+				</svg>
 			</span>
 		</span>
-		${checkboxes[i]}
-	</label>
-`;
+	</span>
+	${dice}
+</label>`)).join('')
 }
---
+++ kolejowy-szlak/test/js/dirtree.js
@@ -0,0 +1,179 @@
+dirtree = [
+	{
+		name: "base",
+		path: "/base"
+	},
+	{ 
+		name: "arcade",
+		path: "/arcade",
+		child: [
+			{
+			name: "galactic-invaders",
+			path: "/arcade/galactic-invaders"
+			},
+			{
+			name: "pluck-man",
+			path: "/arcade/pluck-man"	
+			},
+			{
+			name: "rainbow",
+			path: "/arcade/rainbow"
+			},
+			{
+			name: "tetromino",
+			path: "/arcade/teromino
+			}
+		]
+	},
+	{  
+		name: "blazing-red",
+		path: "/blazing-red",
+		child: [
+			{
+			name: "lava",
+			path: "/blazing-red/lava"
+			},
+			{
+			name: "meteor",
+			path: "/blazing-red/meteor"	
+			}
+		]
+	},
+	{  
+		name: "deep-blue",
+		path: "/deep-blue",
+		child: [
+			{
+			name: "lake",
+			path: "/deep-blue/lake"
+			},
+			{
+			name: "river",
+			path: "/deep-blue/river"	
+			}
+		]
+	},
+	{ 
+		name: "eldritch",
+		path: "/eldritch",
+		child: [
+			{
+			name: "investigation",
+			path: "/eldritch/investigation"
+			},
+			{
+			name: "portal",
+			path: "/eldritch/portal"	
+			},
+			{
+			name: "ritual",
+			path: "/eldritch/ritual"
+			},
+			{
+			name: "tentacles",
+			path: "/eldritch/tentacles
+			}
+		]
+	},
+	{ 
+		name: "electricity",
+		path: "/electricity",
+		child: [
+			{
+			name: "power-grid",
+			path: "/electricity/power-grid"
+			},
+			{
+			name: "street-lamps",
+			path: "/electricity/street-lamps
+			}
+		]
+	},
+	{ 
+		name: "engineer",
+		path: "/engineer",
+		child: [
+			{
+			name: "construction",
+			path: "/engineer/construction"
+			},
+			{
+			name: "renovation",
+			path: "/engineer/renovation"	
+			},
+			{
+			name: "separation",
+			path: "/engineer/separation"
+			},
+			{
+			name: "special",
+			path: "/engineer/special
+			}
+		]
+	},
+	{ 
+		name: "futuristic",
+		path: "/futuristic",
+		child: [
+			{
+			name: "alien-farmer",
+			path: "/futuristic/alien-farmer"
+			},
+			{
+			name: "city-builder",
+			path: "/futuristic/city-builder"	
+			},
+			{
+			name: "superconnection",
+			path: "/futuristic/superconnection
+			}
+		]
+	},
+	{ 
+		name: "lush-green",
+		path: "/lush-green",
+		child: [
+			{
+			name: "forest",
+			path: "/lush-green/forest"
+			},
+			{
+			name: "trails",
+			path: "/lush-green/trails
+			}
+		]
+	},
+	{ 
+		name: "shining-yellow",
+		path: "/shining-yellow",
+		child: [
+			{
+			name: "canyon",
+			path: "/shining-yellow/canyon"
+			},
+			{
+			name: "desert",
+			path: "/shining-yellow/desert
+			}
+		]
+	},
+	{ 
+		name: "sky",
+		path: "/sky",
+		child: [
+			{
+			name: "airport",
+			path: "/sky/airport"
+			},
+			{
+			name: "weather",
+			path: "/sky/weather
+			}
+		]
+	},
+	{
+		name: "underground",
+		path: "/underground"
+	}
+]
+
